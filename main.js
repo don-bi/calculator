4f9a318810f25c9cbcec0c7a38b754f5cb435ac1
don-bi <dbi30@stuy.edu>
2022-08-24 17:30:43 -0400

    adde the rest of the buttons and made styling changes

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+//vars containing the rows 
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+
 //adds the numbers buttons to the calculator
-setNumbers(document.querySelector(".one"),7,9);
-setNumbers(document.querySelector(".two"),4,6);
-setNumbers(document.querySelector(".three"),1,3);
-setNumbers(document.querySelector(".four"),0,0);
+setNumbers(one,7,9);
+setNumbers(two,4,6);
+setNumbers(three,1,3);
+setNumbers(four,0,0);
 
 //function to add number buttons to each row
 function setNumbers(row, start, end) {
@@ -13,3 +19,19 @@ function setNumbers(row, start, end) {
         row.appendChild(btn);
     }
 }
+
+//function to add the other buttons
+function addButton(row, name, text){
+    const btn = document.createElement("button");
+    btn.classList.add(`${name}`);
+    btn.textContent = `${text}`;
+    row.appendChild(btn);
+}
+
+//adds the rest of the buttons
+addButton(one, "multiply", "×");
+addButton(two, "divide", "÷");
+addButton(three, "add", "+");
+addButton(four, "decimal", ".");
+addButton(four, "equal", "=");
+addButton(four, "subtract", "-");
