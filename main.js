ae91b5bb68312e79f6b8bd5d78483e5b87e2af05
don-bi <dbi30@stuy.edu>
2022-08-24 18:49:49 -0400

    made operation buttons work

--- main.js
+++ main.js
@@ -35,12 +35,12 @@ function addButton(row, name, text){
 }
 
 //adds the rest of the buttons
-addButton(one, "multiply", "×");
-addButton(two, "divide", "÷");
-addButton(three, "add", "+");
+addButton(one, "operation", "×");
+addButton(two, "operation", "÷");
+addButton(three, "operation", "+");
 addButton(four, "decimal", ".");
 addButton(four, "equal", "=");
-addButton(four, "subtract", "-");
+addButton(four, "operation", "-");
 
 //does intial display update
 updateDisplay();
@@ -55,15 +55,66 @@ function updateDisplay(){
 
 function includeNum(e){
     if (currentnum.length < 15) { 
-        currentnum += this.textContent;
+        if (currentnum == `0`) {
+            currentnum = this.textContent;
+        } else {
+            currentnum += this.textContent;
+        }
         updateDisplay();
     }
     this.classList.add("pressed");
 }
 
+function addOperation(e){
+    const inputEndsWithOp = currentnum.charAt(currentnum.length-1) == currentop;
+    //if the input currently has no operation or ends with one, it will switch the operation to it
+    if (currentop == `` || inputEndsWithOp) {
+        currentop = this.textContent; 
+        if (inputEndsWithOp) {
+            currentnum = currentnum.substring(0,currentnum.length-1) + currentop;
+        } else {
+            currentnum += currentop;
+        }
+        updateDisplay();
+        this.classList.add("pressed");
+        console.log("doiing the first");
+    } 
+    //otherwise, do the expression
+    else {
+        operate(this.textContent);
+        console.log("doing the second");
+    }
+}
+
+function operate(newop){
+    let data = currentnum.split(currentop);
+    //for the case when the first number is negative
+    if (currentop == currentnum.charAt(0)) {
+        data = currentnum.substring(1).split(currentop);
+        data[0] = -data[0];
+    }
+    answer = currentnum;
+    const first = parseInt(data[0]);
+    const second = parseInt(data[1]);
+    //does the operation depending on which one it is
+    switch(currentop){
+        case `×`: currentnum = first * second; break;
+        case `÷`: currentnum = first / second; break;
+        case `+`: currentnum = first + second; break;
+        case `-`: currentnum = first - second; break;
+        default: currentnum = 0;
+    }
+    currentnum += newop;
+    currentop = newop;
+    updateDisplay();
+}
+
 //adds eventlistener for when numbers are pressed to add it to the input string
 const numbuttons = document.querySelectorAll(".number");
 numbuttons.forEach(num => num.addEventListener("click",includeNum));
+
+const opbuttons = document.querySelectorAll(".operation");
+opbuttons.forEach(op => op.addEventListener("click",addOperation))
 
 
 //removes pressed from buttons after animation
